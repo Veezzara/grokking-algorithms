9e6cfc8002a1fc06037177fd1cf761e41a0fd18b
Max Zhabin <veezzara@gmail.com>
2025-01-17 20:07:21 +0400

    greatest common divisor

---
+++ greatest-common-divisor/index.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number[]} Returns a pair of number as a sorted array (in an ascending order).
+ */
+const swapToAsc = (a, b) => {
+  if (b < a) {
+    return [b, a];
+  }
+
+  return [a, b];
+};
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number} Greatest common divisor.
+ */
+const greatestCommonDivisor = (a, b) => {
+  const [c, d] = swapToAsc(a, b);
+  const remainder = d % c;
+
+  if (remainder === 0) {
+    return c;
+  }
+
+  return greatestCommonDivisor(c, remainder);
+};
+
+console.log(greatestCommonDivisor(1680, 640));
+console.log(greatestCommonDivisor(640, 1680));
